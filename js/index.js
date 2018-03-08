2dd91db8f4be9ec642e674df4505012b3e87516d
Joshua Pouliot <joshuapouliot27@gmail.com>
2018-03-07 20:14:32 -0500

    Updated website.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,26 @@
+
+
 function tabClicked(contentType) {
 
-    var i, tabbedContent, newContent, tabContent;
+    var i, n, tabbedContent, newContent;
 
     tabbedContent = document.getElementsByClassName("tabbedContent");
-    console.log(tabbedContent.size)
-    for (tabContent in tabbedContent) {
-        console.log(tabContent.className)
-        console.log(tabContent.id)
+    for (n=0; n<tabbedContent.length; n++) {
+        tabbedContent.item(n).style.display = "none";
+    }
+    console.log(Modernizr.svg)
+    if (Modernizr.svg) {
+        var svgElements, svgAltElements;
+        svgElements = document.getElementsByTagName("svg")
+        svgAltElements = document.getElementsByClassName("svgAlt")
+        for (n = 0; n< svgElements.length; n++) {
+            svgElements[n].style.display = "none";
+        }
+        for (n = 0; n < svgAltElements.length; n++) {
+            svgAltElements[n].style.display = "inherit";
+        }
     }
 
     tabContent = document.getElementById(contentType);
-    tabContent.style.display = "initial";
+    tabContent.style.display = "inherit";
 }
