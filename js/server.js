00936313808e6a3889de853881962e2607547a6d
Joshua Pouliot <joshuapouliot27@gmail.com>
2018-04-10 13:09:22 -0400

    Updated website.

--- js/server.js
+++ js/server.js
@@ -22,12 +22,29 @@ function checkMessage(message) {
     return false;
 }
 
+function makeEmptyJSON() {
+    var JSONData = JSON.parse("");
+    JSONData.moving_forward = false;
+    JSONData.moving_right = false;
+    JSONData.moving_left = false;
+    JSONData.moving_backward = false;
+    JSONData.current_latitude = null;
+    JSONData.current_longitude = null;
+    JSONData.current_direction_degrees = null;
+    JSONData.current_distance_ahead = null;
+    JSONData.stop_everything = null;
+    return JSONData;
+}
+
 function getJSONData() {
+    if (!fs.existsSync(json_filename)) {
+        saveJSONFile(make_empty_json);
+    }
     var JSONText = fs.readFileSync(json_filename).toString();
     var JSONData = JSON.parse(jsonText);
     return JSONData.stringify();
 }
 
 function saveJSONFile(message) {
-    fs.writeFile(json_filename, JSON.parse(message).stringify())
+    fs.writeFileSync(json_filename, JSON.parse(message).stringify())
 }
